09dbc1585ba704ad6c0e950df3f32f4143d363d7
Pooja Akshantal <pooja.akshantal@gmail.com>
2013-07-14 20:59:47 +0530

    modified subheading

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  var fs = require('fs')
- var contents = fs.readFileSync("index.html", "utf-8");
+ var contents = fs.re adFileSync("index.html", "utf-8");
  response.send(contents);
 });
 
